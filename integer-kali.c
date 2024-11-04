0b7a66dcf45673e0673686fe11977e4f12977960
vermilion10 <fyassarahman@gmail.com>
2024-11-04 07:10:28 +0000

    basic io 1 with c lang

---
+++ integer-kali.c
@@ -0,0 +1,15 @@
+/* 
+Description : multiplication of two integer
+Author : vermilion10
+date : november 2024
+*/
+
+#include<stdio.h>
+
+int main(){
+    int X, Y, Result;
+    scanf("%d %d", &X, &Y);
+    Result = X * Y;
+    printf("%d", Result);
+    return 0;
+}
