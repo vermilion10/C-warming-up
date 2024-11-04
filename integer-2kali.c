9fd937a493615b0d685237c4c12b3602bf46845e
vermilion10 <fyassarahman@gmail.com>
2024-11-04 04:52:21 +0000

    basic i/o 1

---
+++ integer-2kali.c
@@ -0,0 +1,15 @@
+/* 
+Deskripsi : menampilkan bilangan dua kali lipat
+Nama /Author : Fathin Yassarahman
+Tanggal/versi : 21 Oktober 2024
+*/
+
+#include<stdio.h>
+
+int main(){
+    int X, Y, Result;
+    scanf("%d %d", &X, &Y);
+    Result = X * 2;
+    printf("%d", Result);
+    return 0;
+}
