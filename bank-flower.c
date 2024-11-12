344fbf081a7875b7792ff06685cdfedc6ffbfd6d
vermilion10 <fathinyassarahmaan@gmail.com>
2024-11-12 18:42:06 +0700

    addition

--- bank-flower.c
+++ bank-flower.c
@@ -7,7 +7,10 @@ date: november 2024
 #include<stdio.h>
 int main(){
     float biaya_awal, persen_bunga, total_biaya;
-    scanf("%f %f", &biaya_awal, &persen_bunga);
+    printf("input biaya awal: ");
+    scanf("%f", &biaya_awal);
+    printf("input persen bunga: ");
+    scanf("%f", &persen_bunga);
     total_biaya = biaya_awal + (biaya_awal * persen_bunga / 100);
     printf("%.2f", total_biaya);
     return 0;
