20c304cc3a87d6e9c5047b016af2a47f34629a8e
vermilion10 <fathinyassarahmaan@gmail.com>
2024-11-14 16:10:41 +0700

    addition

--- integer_x_to_y.c
+++ integer_x_to_y.c
@@ -6,7 +6,10 @@ date: November 2024
 #include<stdio.h>
 int main(){
     int X, Y;
-    scanf("%d %d", &X, &Y);
+    printf("input X: ");
+    scanf("%d", &X);
+    printf("input Y: ");
+    scanf("%d", &Y);
     for(int i = X; i <= Y; i++){
         printf("%d", i);
         if (i < Y){
